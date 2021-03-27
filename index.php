f7dce50a3d3cbbc801f7a4dcab302ac032791a51
SaranKumar <55644959+Sarankumar18@users.noreply.github.com>
2021-03-27 18:56:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
- echo "Hello World ";
+
 //Define your host here.
 $HostName = "sql207.epizy.com";
  
@@ -14,59 +14,21 @@ $HostPass = "QIAi0ePi11O";
 
 $conn = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
 
-$json = file_get_contents('php://input');
+ $username = $_POST['username'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
 
-$obj = json_decode($json,true);
+ $sql = "INSERT INTO user(username,email,password) VALUES ('$username','$email','$password')";
 
-$username = $obj['username'];
- 
-// Populate User email from JSON $obj array and store into $email.
-$email = $obj['email'];
- 
-// Populate Password from JSON $obj array and store into $password.
-$password = $obj['password'];
-$CheckSQL = "SELECT * FROM user WHERE email='$email'";
+ $R = $conn->query($sql);
 
-// Executing SQL Query.
-$check = mysqli_fetch_array(mysqli_query($con,$CheckSQL));
-
-
-if(isset($check)){
-
- $EmailExistMSG = 'Email Already Exist, Please Try Again !!!';
- 
- // Converting the message into JSON format.
-$EmailExistJson = json_encode($EmailExistMSG);
- 
-// Echo the message.
- echo $EmailExistJson ; 
-
+ if($R){
+     $M = "Success";
  }
  else{
- 
-    // Creating SQL query and insert the record into MySQL database table.
-   $Sql_Query = "insert into user (username,email,password) values ('$username','$email','$password')";
-    
-    
-    if(mysqli_query($con,$Sql_Query)){
-    
-    // If the record inserted successfully then show the message.
-   $MSG = 'User Registered Successfully' ;
-    
-   // Converting the message into JSON format.
-   $json = json_encode($MSG);
-    
-   // Echo the message.
-    echo $json ;
-    
-    }
-    else{
-    
-    echo 'Try Again';
-    
-    }
-    }
-    mysqli_close($con);
+     $M = "Failed";
+ }
+ echo($M);
 
  
 ?>
