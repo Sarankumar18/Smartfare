52cfe6c5c1c9184a785dd35504103c7a8dbe8863
SaranKumar <55644959+Sarankumar18@users.noreply.github.com>
2021-04-20 20:22:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,50 @@
 <?php
-echo "Hello";
-//Define your host here.
-$HostName = "sql207.epizy.com";
+
+
  
 //Define your database name here.
-$DatabaseName = "epiz_28244514_smartfare";
+$HostName = "localhost";
+ 
+//Define your database name here.
+$DatabaseName = "id12617037_smartfare";
  
 //Define your database username here.
-$HostUser = "epiz_28244514";
+$HostUser = "id12617037_smartfaredb";
  
 //Define your database password here.
-$HostPass = "QIAi0ePi11O";
+$HostPass = "*FL0=VeeQyb^6*VX";
 
 $conn = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
 
- $username = $_POST['username'];
- $email = $_POST['email'];
- $password = $_POST['password'];
+$EncodedData = file_get_contents('php://input');
+$DecodedData = json_decode($EncodedData,true);
 
- $sql = "INSERT INTO user(username,email,password) VALUES ('$username','$email','$password')";
+$eid = $DecodedData['eid'];
+$password = $DecodedData['password'];
 
- $R = $conn->query($sql);
+$Login  = "SELECT * FROM employee WHERE eid = '$eid'";
+$data = $conn->query($Login);
+$status;
+if (mysqli_num_rows($data) > 0) {
+while($row = mysqli_fetch_assoc($data))
+    {
+        $Eid = $row['eid'];
+        $Password = $row['password'];
+        $Name = $row['name'];
+        
+    }
+   
+}
+else{
+   $message = "No Employee Found";
 
- if($R){
-     $M = "Success";
- }
- else{
-     $M = "Failed";
- }
- echo($M);
+}
+  if($password != $Password){
+    $message = "Incorrect Password";
+  }
 
- 
+$Response[] = array("eid"=> $Eid,"password"=> $Password, "name"=> $Name,"status"=> $status);
+echo json_encode($Response);
+
+
 ?>
